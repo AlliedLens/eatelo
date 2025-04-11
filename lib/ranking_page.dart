a00a04f8d8b841a15209f198d9e102c00f87d0b0
chinmayib-ops <chinmayi.mitmpl2022@learner.manipal.edu>
2025-04-11 16:09:53 +0530

    style: Add box shadow to rankings container

--- lib/ranking_page.dart
+++ lib/ranking_page.dart
@@ -90,6 +90,14 @@ class _RankingPageState extends State<RankingPage> {
                 decoration: BoxDecoration(
                   color: const Color(0xFFD9D9D9),
                   borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
                 ),
                 child: Column(
                   children: [
